612af835b48d7d93b703d8e2fefe3f71fd649833
Krystelle Rupnarain <104322527+KrystelleR@users.noreply.github.com>
2024-02-13 10:13:16 +0200

    Variables

---
+++ Fundamentals/3_variables/VariablesNamingRules.java
@@ -0,0 +1,30 @@
+public class  VariablesNamingRules{
+	public static void main(String [] args){
+
+	//Rule1
+	String name = "Krystelle";
+	String surname = "Rupnarain";
+	String phoneNumber = "012 345 6789";
+
+	System.out.println("Welcome back, " + name + " " + surname + "! Your number is saved as: " + phoneNumber);
+
+	//Rule2
+	String student0 = "Aaron";
+	String student1 = "Ghislain-Claude";
+	String student2 = "Samantha";
+	String student3 = "Tumi";
+	String student4 = "Manqoba";
+
+	System.out.println("The 3rd student is: " + student3);
+
+
+	//Rule3
+	int numOfMonths = 12;
+	int numOfWeeks = 52;
+	int numOfDays = 365;
+
+	System.out.println("The number of days within a year are:" + numOfDays);
+	System.out.println("The number of months within a year are:" + numOfMonths);
+	System.out.println("The number of weeks within a year are:" + numOfWeeks);
+	}
+}
