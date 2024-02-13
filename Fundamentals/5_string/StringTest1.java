4c63b63eb648c189392f3f67f3d074ac4218cc2b
Krystelle Rupnarain <104322527+KrystelleR@users.noreply.github.com>
2024-02-13 13:43:46 +0200

    String lesson p2

--- Fundamentals/5_string/StringTest1.java
+++ Fundamentals/5_string/StringTest1.java
@@ -1,8 +1,8 @@
 public class StringTest1{
     public static void main(String [] args){
-        String name = "John";
-        String surname = "Smith";
-		String fullName = name+surname;
+        String name = "Krystelle";
+        String surname = "Rupnarain";
+		String fullName = name+" " +surname;
 
         System.out.println("Full Name :"+fullName);
     }
